459e756c9d51c398a13fc013751a92436a5fae1c
Hui Peng <benquike@163.com>
2015-05-30 18:42:01 +0900

    [DEV]Setup package manager to make it download packages automatically

--- init.el
+++ init.el
@@ -23,3 +23,37 @@
   (load-files-wildcards (format "%s/*-init.el" load-dir))
   (load-files-wildcards (format "%s/**/*-init.el" load-dir))
 )
+
+;;; package setup
+;;; refer to http://emacswiki.org/emacs/ELPA
+
+(require 'package)
+(package-initialize)
+
+;;; setup the archives
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t); Org-mode's repository
+
+(defvar my-packages-to-install
+  '(ggtags)
+  "The packages Emacs will try to install when it starts up.")
+
+(defun my-packages-installed-p ()
+  (loop for p in my-packages-to-install
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t))
+)
+
+(unless (my-packages-installed-p)
+  ;; check for new packages (package versions)
+  (package-refresh-contents)
+
+  (message "Emacs is checking whether the packages are installed...")
+  (dolist (p my-packages-to-install)
+    (when (not (package-installed-p p))
+      (package-install p))
+  )
+  (message "Checking done")
+)
