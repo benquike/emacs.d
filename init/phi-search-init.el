648ef02ed4a5674d9d63cd830c0bf5bf37ba0621
Hui Peng <benquike@163.com>
2017-06-30 15:18:43 -0400

    Add init/phi-search-init.el

---
+++ init/phi-search-init.el
@@ -0,0 +1,10 @@
+;; (require 'phi-search)
+
+;; (global-set-key (kbd "C-s") 'phi-search)
+;; (global-set-key (kbd "C-r") 'phi-search-backward)
+
+;; (require 'phi-replace)
+
+;; (global-set-key (kbd "M-%") 'phi-replace-query)
+;; (define-key phi-search-default-map (kbd "<") 'phi-search-again-or-previous)
+;; (define-key phi-search-default-map (kbd ">") 'phi-search-again-or-next)
