88173fc951aff60f582bba33042e2950b490835c
Hui Peng <benquike@163.com>
2017-06-30 15:19:03 -0400

    Add init/php-mode-init.el

---
+++ init/php-mode-init.el
@@ -0,0 +1,5 @@
+(require 'php-mode)
+;; (add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
+;; (add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
